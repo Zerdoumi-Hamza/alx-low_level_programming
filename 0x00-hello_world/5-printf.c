7d97f25563cf3ac0571faa612c667dbdc81124f5
Zerdoumi-Hamza <zerdoumi.hamza.um5@gmail.com>
2023-12-19 02:34:17 +0100

    update file 5-*

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,12 @@
 #include <stdio.h> 
+
+/**
+* main - principale function
+*
+*here where the entire programme start and from here excute
+*Return: 0 if succeed
+*/
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+* main - main function
+*
+*Return:  return 0 if succeed
+*/
+
+int main(void)
+{
+	char 
