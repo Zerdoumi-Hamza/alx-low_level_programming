15c1481733713b431d6b02cadb841adc4a826161
Zerdoumi-Hamza <zerdoumi.hamza.um5@gmail.com>
2023-12-22 19:44:20 +0100

    Update 2-file

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,15 +4,21 @@
 *print_alphabet_x10();
 *
 *Return: Don't return  anything, it is a print function no less no more. $
-*/ 
+*/
 
 void print_alphabet_x10(void)
 {
 	int i = 0;
+	int c = 97;
+
 	while (i < 10)
 	{
-		print_alphabet();
-		i++;
+		while ((c >= 97) && (c <= 122))
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
 	}
 	return (0);
 }
