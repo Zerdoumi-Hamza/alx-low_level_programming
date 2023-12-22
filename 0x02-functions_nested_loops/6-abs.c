ece205a2647b85bf58ac0c9d2d6b3c32ee9596ca
Zerdoumi-Hamza <zerdoumi.hamza.um5@gmail.com>
2023-12-22 22:33:28 +0100

    Upadate file 6-* and creat file contine function  return last digit.

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-*_abs  - a function that prints the absolute of an integer. 
+*_abs  - a function that prints the absolute of an integer.
 *@n: the number need to cheeck it.
 *
 *Return: 1 if > 0 0 if 0 and -1 if < 0.
@@ -11,6 +11,6 @@ int _abs(int n)
 {
 	if (n >= 0)
 		return (n);
-	else (n < 0)
+	else
 		return (-n);
 }
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* print_last_digit - a function prints the last digit of a number.
+*@n: number
+*Return: the laste digit.
+*/
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
