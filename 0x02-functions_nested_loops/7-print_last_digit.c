76dc8c8ea370b193cbb40f68944b4c114e33eb28
Zerdoumi-Hamza <zerdoumi.hamza.um5@gmail.com>
2023-12-23 11:27:51 +0100

    Update Switch case in file 7-

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
 * print_last_digit - a function prints the last digit of a number.
 *@n: number
 *Return: the laste digit.
 */
-
 int print_last_digit(int n)
 {
 	int x = n % 10;
@@ -16,26 +14,38 @@ int print_last_digit(int n)
 	{
 		case 0:
 			_putchar(48);
+			break;
 		case 1:
 			_putchar(49);
+			break;
 		case 2:
 			_putchar(50);
+			break;
 		case 3:
 			_putchar(51);
+			break;
 		case 4:
 			_putchar(52);
+			break;
 		case 5:
 			_putchar(53);
+			break;
 		case 6:
 			_putchar(54);
+			break;
 		case 7:
 			_putchar(55);
+			break;
 		case 8:
 			_putchar(56);
+			break;
 		case 9:
 			_putchar(57);
+			break;
 		default:
 			_putchar(122);
+			break;
 	}
+	_putchar('\n');
 	return (x);
 }
