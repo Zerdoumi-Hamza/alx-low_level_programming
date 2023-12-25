b082f369e9fa37b904a14fe418a79f261e267748
Zerdoumi-Hamza <zerdoumi.hamza.um5@gmail.com>
2023-12-25 16:20:11 +0100

    adding c file that contain function for printing the number from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*print_numbers - printing the number from 0 to 9.
+*
+*Return: is a void function, just for printing.
+*/
+
+void print_numbers(void)
+{
+	int c = 48;
+
+	for (; c <= 57;)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,6 +3,8 @@
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+int _putchar(char c);
+void print_numbers(void);
 #endif /* HEADER_FILE*/
 
 
