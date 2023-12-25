6fa780eada8858be78853a25cd88a427736b8c40
Zerdoumi-Hamza <zerdoumi.hamza.um5@gmail.com>
2023-12-25 23:22:01 +0100

    Update file 8-

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,18 +10,18 @@ void print_square(int size)
 	int i;
 	int j;
 
-	if ( size > 0)
-	{	
+	if (size > 0)
+	{
 		for (i = 0; i < size; i++)
 		{
 			for (j = 0; j < size; j++)
 			{
 				_putchar(35);
-			} 
+			}
 			_putchar('\n');
 		}
 	}
-	else 
+	else
 	{
 		_putchar('\n');
 	}
