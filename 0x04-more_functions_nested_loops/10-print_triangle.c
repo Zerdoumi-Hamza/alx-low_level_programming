2ed2d4565a52adcdb732ef763b2e440bd2f9d79d
Zerdoumi-Hamza <zerdoumi.hamza.um5@gmail.com>
2023-12-26 00:37:31 +0100

    Update file 10-

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
-* print_triangle - a function print a triangle with # according to size enter it by user.
-*@size: The size of triangel. 
+* print_triangle - a function print a triangle with # according
+* to size enter it by user.
+*@size: The size of triangel.
 *Return: No return.
 */
 
@@ -11,7 +12,7 @@ void print_triangle(int size)
 	int i;
 	int j;
 	int k;
-	
+
 	if (size <= 0)
 	{
 		goto justnewline;
@@ -21,7 +22,7 @@ void print_triangle(int size)
 		for (j = 1; j <= size - i; j++)
 		{
 			_putchar(32);
-		}	
+		}
 		for (k = j; k <= size; k++)
 		{
 			_putchar(35);
