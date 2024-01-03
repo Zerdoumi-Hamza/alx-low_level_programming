0a661af3a7a1eee86e62d74b7b65b9b1fd4552c2
Zerdoumi-Hamza <zerdoumi.hamza.um5@gmail.com>
2024-01-03 22:09:50 +0100

    Update file 3 and the header file main.h

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,14 +1,14 @@
 #include "main.h"
-/**_puts - function prints a string.
+/**
+*_puts - function prints a string.
 *@str: pointer point to the value in adress of argument.
-*
 *Return: Nothing.
 */
-
 void _puts(char *str)
 {
 	int i = 0;
-	while(str[i])
+
+	while (str[i])
 	{
 		_putchar(str[i]);
 		i++;
