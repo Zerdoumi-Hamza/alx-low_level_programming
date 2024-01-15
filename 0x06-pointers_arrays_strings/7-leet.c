536fe6da8ace80a3fb6dcddca707e76a1f9117e3
Zerdoumi-Hamza <zerdoumi.hamza.um5@gmail.com>
2024-01-15 10:07:56 +0100

    Update file 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,17 +7,19 @@
 
 char *leet(char *s)
 {
-	int i = 0, j;
+	int i = 0, j = 0;
 	char upper_[] = "AEOTL";
 	char lower_[] = "aeotl";
 	int number[] = {52, 51, 48, 55, 49};
 
 	while (s[i] != '\0')
 	{
-		for (j = 0; j <= sizeof(number) / sizeof(int); j++)
+		/*for (j = 0; j <= sizeof(number) / sizeof(int); j++)*/
+		while (s[j] != '\0')
 		{
 			if (s[i] == upper_[j] || s[i] == lower_[j])
 				s[i] = number[j];
+			j++;
 		}
 		i++;
 	}
