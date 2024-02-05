073b9af265553695c203b655a1128fb35d53d0ee
Zerdoumi-Hamza <zerdoumi.hamza.um5@gmail.com>
2024-02-05 08:45:40 +0100

    Update file 0-*  to suit the Betty stile

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-/*#include "main.h"*/
-#include "_putchar.c"
-/*
-*_puts_recursion -  Recursive function that print the string.
-*s: the string suposed to sent to the function.
+#include "main.h"
+
+/**
+* _puts_recursion -  Recursive function that print the string.
+*@s: the string suposed to sent to the function.
 *Return: Nothing.
 */
 
